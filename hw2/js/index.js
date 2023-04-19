e902d8600dc692f5cbf4892d5c52176e2a160363
noskinnen <130252943+noskinnen@users.noreply.github.com>
2023-04-20 01:33:59 +0300

    add new task

---
+++ hw2/js/index.js
@@ -0,0 +1,39 @@
+// 1
+const a = 20;
+alert('10');
+
+// 2 какой год выпуска первого iPhone
+let iPhone;
+iPhone = "29 июня 2007 года";
+alert(iPhone);
+
+// 3
+let be = 'BrendanEich';
+alert (be);
+
+// 4
+let userName = prompt('Введите как вас зовут?');
+alert('Добро пожаловать на сайт ${userName}');
+
+// 5
+
+// 6
+
+// 7
+let age;
+// age = prompt('сколько вам лет?');
+alert(age);
+
+// 8
+
+// 9.0
+
+// 9.1
+
+// 9.2
+
+// 9.3
+
+// 9.4
+
+// 10
