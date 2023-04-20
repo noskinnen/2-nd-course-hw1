9c843189dcb1695c7f9d4160ba324a5d1f9f5632
noskinnen <130252943+noskinnen@users.noreply.github.com>
2023-04-20 10:57:07 +0300

    add new task

--- hw2/js/index.js
+++ hw2/js/index.js
@@ -1,39 +1,69 @@
 // 1
-const a = 20;
+let (a) = 20;
 alert('10');
 
-// 2 какой год выпуска первого iPhone
+// 2 
 let iPhone;
 iPhone = "29 июня 2007 года";
 alert(iPhone);
 
 // 3
-let be = 'BrendanEich';
-alert (be);
+let be = "BrendanEiЧ";
+alert(be);
 
 // 4
-let userName = prompt('Введите как вас зовут?');
-alert('Добро пожаловать на сайт ${userName}');
+a.valueOf(10) 
+b.toString(2) 
+alert(result)
 
 // 5
+let result = (2**5);
+alert('result')
 
 // 6
+let a = 9;
+let b = 2;
+alert(a / b);
 
 // 7
-let age;
-// age = prompt('сколько вам лет?');
-alert(age);
+let num = 1;
+num = num + 5;
+num = num - 3;
+num = num * 7;
+num = num / 3;
+num = num + 1;
+num = num - 1;
+alert(num);
 
 // 8
+let age;
+age = prompt('сколько вам лет?');
+alert(age);
 
 // 9.0
+const person = {    
+name: 'Саша',
+age: 35,
+isAdmin: ("любой текст"),
+}
 
 // 9.1
+let user = {cityOfResidence};
 
 // 9.2
+console.log(user['name']); 
+console.log(user['age']); 
+user['isPedigree'] = true;
+delete user['weight'];
 
 // 9.3
+let user = prompt("city of residence");
+alert(age);
 
 // 9.4
+let info = prompt("Какую информацию хотите узнать о пользователе?");
+alert(info);
 
 // 10
+const name = prompt('Введите ваше имя');
+alert(Привет[name]);
