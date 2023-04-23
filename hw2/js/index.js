f7a8ac98d65dfedb659775b1ca1a0caffd34a523
noskinnen <130252943+noskinnen@users.noreply.github.com>
2023-04-24 02:26:33 +0300

    add new folder

--- hw2/js/index.js
+++ hw2/js/index.js
@@ -1,69 +1,77 @@
 // 1
-let (a) = 20;
-alert('10');
+let a = 20;
+alert(a);
+b = 10;
+alert(b);
 
 // 2 
-let iPhone;
+const newLocal = iPhone;
 iPhone = "29 июня 2007 года";
 alert(iPhone);
 
 // 3
-let be = "BrendanEiЧ";
-alert(be);
+const Brendan = "[BrendanEich]";
+alert(Brendan);
 
 // 4
-a.valueOf(10) 
-b.toString(2) 
-alert(result)
+'use strict';
+let x = 10;
+let y = 2;
+alert(`${x} + ${y} = ${x + y}`); // 10 + 2 = 12
+alert(`${x} - ${y} = ${x - y}`); // 10 - 2 = 8
+alert(`${x} * ${y} = ${x * y}`); // 10 * 2 = 20
+alert(`${x} / ${y} = ${x / y}`); // 10 / 2 = 5
 
-// 5
+// 5 
 let result = (2**5);
-alert('result')
+alert('result');
 
-// 6
+// 6 
 let a = 9;
 let b = 2;
-alert(a / b);
+alert(`${a} / ${b} = ${a / b}`); // 9 / 2 = 4.5
 
-// 7
-let num = 1;
-num = num + 5;
-num = num - 3;
-num = num * 7;
-num = num / 3;
-num = num + 1;
-num = num - 1;
-alert(num);
+// 7 
+let n = 1;
+n += 5; 
+n -= 3; 
+n *= 7; 
+n /= 3; 
+n += 1; 
+n -= 1; 
+alert( n ); // 7
 
-// 8
+// 8 
 let age;
 age = prompt('сколько вам лет?');
 alert(age);
 
-// 9.0
-const person = {    
-name: 'Саша',
+// 9.0 
+const person  = 
+{    
+name: "Cаша",
 age: 35,
 isAdmin: ("любой текст"),
 }
+alert (person);
 
 // 9.1
-let user = {cityOfResidence};
+let user = 'city Of Residence';
+alert(user);
 
 // 9.2
-console.log(user['name']); 
-console.log(user['age']); 
-user['isPedigree'] = true;
-delete user['weight'];
+let age = prompt('измените ваш возраст');
+alert(`Тебе ${age} лет!`);
 
 // 9.3
-let user = prompt("city of residence");
-alert(age);
+let user = 'city Of Residence';
+delete user.cityOfResidence;
+alert(user);
 
 // 9.4
 let info = prompt("Какую информацию хотите узнать о пользователе?");
 alert(info);
 
 // 10
-const name = prompt('Введите ваше имя');
-alert(Привет[name]);
+let name = prompt('Введите ваше имя');
+alert(`Привет ${name}!`);
